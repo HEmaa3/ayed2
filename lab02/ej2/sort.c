7699fae77c915ac4bb0490bdd266c99dc10d073d
HEmaa3 <110110471+HEmaa3@users.noreply.github.com>
2022-07-27 11:54:10 -0300

    Add files via upload

---
+++ lab02/ej2/sort.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
+ /* needs implementation */
+
+    /* no implementes partition, ya está implementado en sort_helpers.o
+       (no se puede leer, pero en sort_helpers.h vas a encontrar información
+        para saber cómo usarlo)
+    */
+    unsigned int ppiv;
+    if (der > izq){
+        ppiv = partition(a,izq,der);
+        quick_sort_rec(a,izq,ppiv);
+        quick_sort_rec(a,ppiv+1,der);
+    }
+
+}
+
+void quick_sort(int a[], unsigned int length) {
+    quick_sort_rec(a, 0u, (length == 0u) ? 0u : length - 1u); // ? operador ternario -> c ? 2 : 3 (se lee de la siguiente manera: si c es True entonces vale 2 si c es False entonces vale 3)
+}
+
---
+++ lab02/ej4/sort.c
@@ -0,0 +1,84 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+static unsigned int min_pos_from(int a[], unsigned int i, unsigned int length) {
+    unsigned int min_pos = i;
+    for (unsigned int j = i + 1; j < length; ++j) {
+        if (!goes_before(a[min_pos],a[j])) {
+            min_pos = j;
+        }
+    }
+    return (min_pos);
+}
+
+void selection_sort(int a[], unsigned int length) {
+    for (unsigned int i = 0u; i < length; ++i) {
+        unsigned int min_pos = min_pos_from(a, i, length);
+        swap(a, i, min_pos);
+    }
+}
+
+
+static void insert(int a[], unsigned int i) {
+    /* copiá acá la implementación que hiciste en el ejercicio 1 */
+    unsigned int j;
+    j=i;
+
+    while (j>0u && goes_before(a[j],a[j-1u])){ // goes_before toma valor absoluto --- devuelve true si a[j] va antes que a[j-1u]
+        swap(a,j-1u, j);
+        j=j-1u;
+    }
+}
+
+void insertion_sort(int a[], unsigned int length) {
+    for (unsigned int i = 1u; i < length; ++i) {
+        insert(a, i);
+    }
+}
+
+
+static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
+    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int ppiv;
+    unsigned int i, j;
+
+    ppiv = izq;
+    i = izq + 1;
+    j = der;
+
+    while (i <= j){
+        if (goes_before(a[i],a[ppiv])){
+            i=i+1;
+        }
+            else if (goes_before(a[ppiv],a[j])){
+                j=j-1;
+            }
+                else if (!goes_before(a[i],a[ppiv]) && goes_before(a[j],a[ppiv])){
+                    swap(a,i,j);
+                }
+    }                             // ASI SE USAN LAS MULTIPLES GUARDAS DEL IF
+    swap(a,ppiv,j);
+    ppiv=j;
+
+    return ppiv;
+}
+
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
+    /* copiá acá la implementación que hiciste en el ejercicio 2 */
+    unsigned int ppiv;
+    if (der > izq){
+        ppiv = partition(a,izq,der);
+        quick_sort_rec(a,izq,ppiv);
+        quick_sort_rec(a,ppiv+1,der);
+    }
+}
+
+void quick_sort(int a[], unsigned int length) {
+    quick_sort_rec(a, 0u, (length == 0u) ? 0u : length - 1u);
+}
