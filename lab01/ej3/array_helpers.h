0a6826dfdd528c0e07801fcfafe4331944a38b0e
HEmaa3 <110110471+HEmaa3@users.noreply.github.com>
2022-07-27 12:10:04 -0300

    Delete lab01 directory

--- lab01/ej3/array_helpers.h
+++
@@ -1,9 +0,0 @@
-unsigned int array_from_file (int array[], unsigned int max_size, const char* filepath);
-
-void array_dump (int a[], unsigned int length);
-
-/* 
-Basicamente el array_helpers.h es la especificacion del TAD
-Aca escribimos los prototipos de las funciones que vamos a utilizar, es decir que devuelve, el no-
-bre de la funcion, los párametros que toma y por ultimo algun comentario (opcional).
- */
