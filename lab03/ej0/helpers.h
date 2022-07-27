b183dfb31a04c0bd3dfa410768f7dbb2037921e2
HEmaa3 <110110471+HEmaa3@users.noreply.github.com>
2022-07-27 12:10:27 -0300

    Delete lab03 directory

--- lab03/ej0/helpers.h
+++
@@ -1,6 +0,0 @@
-unsigned int data_from_file (const char* path, unsigned int indexes[], char letters[], unsigned int max_size);
-
-char* parse_file(int argc, char* argv[]);
-
-void sort (char sorted[], unsigned int indexes[], char letters[], unsigned int length);
-
